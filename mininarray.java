12adfe08829ce4c04fe1e28e265b46fd683fadfe
miruthula <miruthula183@gmail.com>
2026-07-13 07:32:11 -0700

    Add program to find minimum element in an array

---
+++ mininarray.java
@@ -0,0 +1,16 @@
+public class mininarray {
+    public static void main(String[] args){
+        int[] arr={10,20,30,40,50};
+        int min=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+                
+            }
+
+        }
+        System.out.print(min);
+    }
+    
+}
+
