daf8ce6cd8598e89ea242830b81c6d7605942ded
miruthula <miruthula183@gmail.com>
2026-07-13 07:33:33 -0700

    program to find index of an element in an array

---
+++ findindex.java
@@ -0,0 +1,19 @@
+public class findindex {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int target = 40;
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println(index);
+    }
+}
